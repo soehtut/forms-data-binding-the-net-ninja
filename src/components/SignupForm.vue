8c474e128e217ef5887e8460b40792755a67dd97
SoeHtut <soehtut@escapepixel.com>
2021-01-13 23:27:35 +0630

    Select Fields

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,9 +5,17 @@
 
         <label>Password:</label>
         <input type="password" required="required" v-model="password">
+
+        <label>Role:</label>
+        <select v-model="role">
+            <option value="web developer">Web Developer</option>
+            <option value="web designer">Web Designer</option>
+        </select>
     </form>
+
     <p>Email: {{ email }}</p>
     <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -15,7 +23,8 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: ''
         }
     }
 }
@@ -39,7 +48,7 @@ export default {
         letter-spacing: 1px;
         font-weight: bold;
     }
-    input {
+    input, select {
         display: block;
         padding: 10px 6px;
         width: 100%;
