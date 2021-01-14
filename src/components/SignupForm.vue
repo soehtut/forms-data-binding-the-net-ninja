8001e29970c14397cead050b7fb296cf3d3073d6
SoeHtut <soehtut@escapepixel.com>
2021-01-14 22:08:30 +0630

    Deleting Skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -20,7 +20,7 @@
         <label>Skills:</label>
         <input type="text" v-model="tempSkill" @keyup="addSkill">
         <div v-for="skill in skills" :key="skill" class="pill">
-            {{ skill }}
+            <span @click="deleteSkill(skill)">{{ skill }}</span>
         </div>
 
     </form>
@@ -51,6 +51,11 @@ export default {
                 }
                 this.tempSkill = ''
             }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            })
         }
     }
 }
@@ -89,6 +94,18 @@ export default {
         margin: 0px 10px 0px 0px;
         position: relative;
         top: 2px;
+    }
+    .pill {
+        display: inline-block;
+        margin: 20px 10px 0 0;
+        padding: 6px 12px;
+        background-color: #eee;
+        border-radius: 20px;
+        font-size: 12px;
+        letter-spacing: 1px;
+        font-weight: bold;
+        color: #777;
+        cursor: pointer;
     }
 </style>
 
